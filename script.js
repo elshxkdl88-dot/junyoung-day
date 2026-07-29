3914406a69d5e5a31cd95298fbc75a966cf3dbd1
elshxkdl88-dot <elshxkdl88@gmail.com>
2026-07-29 10:18:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const title = document.getElementById("title");
 const nextBtn = document.getElementById("nextBtn");
 
 const photos = [
-"images/1.jpg",
-"images/2.jpg",
-"images/3.jpg",
+"images/1.png",
+"images/2.png",
+"images/3.png",
 "images/4.jpg",
 "images/5.jpg",
 "images/6.jpg"
